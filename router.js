c6ff26bfdbf29110ae483b3043d5dcbe6c45a25c
AthiraAnil-1996 <101643170+AthiraAnil-1996@users.noreply.github.com>
2022-07-05 18:43:58 +0530

    First

---
+++ router.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+//router.post('/create',(req,res)=>{
+    //req.session.user = req.body.Yname;
+    //console.log(req.session.user);
+    //res.redirect('/route/dashboard');
+    //res.send("Login successful");
+//})
+
+//route for Dashboard
+
+router.post('/dashboard',(req,res)=>{
+    var Yname = req.body.Yname;
+    var Fname = req.body.Fname;
+    res.render('dashboard.ejs',{form_name:Yname,friend:Fname});
+
+})
+
+router.post('/preview',(req,res)=>{
+    //var Yname = req.body.Yname;
+    //var Fname = req.body.Fname;
+    var Y = req.body.Yname;
+    var F = req.body.Fname;
+    res.render('preview.ejs',{form_name:Y,friend:F});
+    //res.send("Login successful");
+    
+
+})
+
+router.post('/message',(req,res)=>{
+    
+    //res.render('preview.ejs',{form_name:Yname,friend:Fname});
+   // alert("Wish sent successful");
+   //res.render('previewall.ejs',);
+   //res.send("Login successful");
+   //popup.alert({
+    //content:'Mail sent successfully'
+   //});
+   //let alert = require('alert'); 
+//alert("message")
+
+
+})
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const path = require('path');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const router = require('./router');
+const app = express();
+
+const port = process.env.PORT||3000;
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+app.set('view engine','ejs');
+
+//load static assets
+
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.use('/static',express.static(path.join(__dirname,'public/assets')));
+
+app.use('/route',router);
+
+//home route
+app.get('/',(req,res)=>{
+    res.render('base');
+
+})
+
+app.get('/route',(req,res)=>{
+   res.render('route/preview');
+
+})
+
+//app.get('/route',(req,res)=>{
+   // res.render('route/previewall');
+
+//})
+
+
+app.listen(port,()=>{ console.log("Listening to the server on http://localhost:3000")});
