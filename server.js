adf671594f53124fbbf5a5ba7d294e727688b07e
AthiraAnil-1996 <101643170+AthiraAnil-1996@users.noreply.github.com>
2022-07-05 21:30:57 +0530

    mail

--- server.js
+++ server.js
@@ -1,40 +1,55 @@
 const express = require('express');
-
+const app = new express();
 const path = require('path');
-const bodyparser = require('body-parser');
-const session = require('express-session');
-const router = require('./router');
-const app = express();
-
-const port = process.env.PORT||3000;
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
-app.set('view engine','ejs');
-
-//load static assets
-
-app.use('/static',express.static(path.join(__dirname,'public')));
-app.use('/static',express.static(path.join(__dirname,'public/assets')));
-
-app.use('/route',router);
-
-//home route
-app.get('/',(req,res)=>{
-    res.render('base',{});
-
-})
+var nodemailer = require('nodemailer');
+const favicon = require('serve-favicon');
+const port = process.env.PORT || 4000;
+//app.use(favicon(path.join('public', 'new-year.png')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+//app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+	res.render('base', {});
+});
 
 app.get('/route',(req,res)=>{
    res.render('route/preview');
 
 })
 
-//app.get('/route',(req,res)=>{
-   // res.render('route/previewall');
+app.post('/submit', (req, res) => {
+	res.render('greetings', { uname: req.body.uname, wname: req.body.wname, email: req.body.email });
+	console.log(req.body.uname);
+	console.log(req.body.wname);
+	console.log(req.body.email);
+	var nodemailer = require('nodemailer');
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'newyearwishmaster@gmail.com',
+			pass: 'iukqdonsjxlwwxdp'
+		}
+	});
 
-//})
+	var mailOptions = {
+		from: 'newyearwishmaster@gmail.com',
+		to: req.body.email,
+		subject: 'Happy New Year 2022',
+		text: ` Hi ${req.body.wname}, ${req.body.uname} wishes you a happy new year 2022`,
+		html: `<h1 style="color:orange;">Hi ${req.body.wname}, ${req.body.uname} wishes you a happy new year 2022</h1>
+		<h1 style="color:orange;">Happy 2022</h1>`
+	};
 
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
+});
 
-app.listen(port,()=>{ 
-   console.log(`Listening to the server on ${port}`);
+app.listen(port, () => {
+	console.log(`Server Ready on ${port}`);
 });
