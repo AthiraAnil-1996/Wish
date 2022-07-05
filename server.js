d61736de5c94269bdf7dc218a1802d6336a2f996
AthiraAnil-1996 <101643170+AthiraAnil-1996@users.noreply.github.com>
2022-07-05 21:18:55 +0530

    ll

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.set('view engine','ejs');
 
 //load static assets
 
-app.use('/static',express.static(path.join(__dirname,'public')));
-app.use('/static',express.static(path.join(__dirname,'public/assets')));
+app.use('/static',express.static(path.join(__dirname,'/public')));
+app.use('/static',express.static(path.join(__dirname,'/public/assets')));
 
 app.use('/route',router);
 
